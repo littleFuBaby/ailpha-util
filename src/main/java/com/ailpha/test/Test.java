f235abda11cd0249ede8236589e5bde55460ae9d
andy <yunsonglife@sina.com>
2022-03-26 09:17:25 +0800

    update Test file

--- src/main/java/com/ailpha/test/Test.java
+++ src/main/java/com/ailpha/test/Test.java
@@ -15,12 +15,13 @@ public class Test {
         int head = 0, tail = 0;
         while (head < s.length() && tail < s.length()) {
             if (s.charAt(head) != s.charAt(tail)) {
-                sum += sum(s.substring(head, tail));
+                sum += sum2(tail - head);
                 head = tail;
                 continue;
             }
             if (tail == s.length() - 1) {
-                sum += sum(s.substring(head));
+                //sum += sum(s.substring(head));
+                sum += sum2(tail - head + 1);
                 break;
             }
             tail++;
@@ -37,6 +38,12 @@ public class Test {
             n++;
         }
         return sum;
+    }
+
+    public static long sum2(int num) {
+        int a1 = 1;
+        int d = 1;
+        return num * a1 + num * (num - 1) * d / 2;
     }
 
 
