f0f262ecf78ff40f2629a60164246d14f80137f8
andy <yunsonglife@sina.com>
2022-11-11 20:33:56 +0800

    add StackOverflowErrorInstance file

---
+++ src/test/java/own/test/StackOverflowErrorInstance.java
@@ -0,0 +1,28 @@
+package own.test;
+
+/**
+ * 栈溢出错误示例
+ *
+ * @author andy
+ */
+public class StackOverflowErrorInstance {
+
+    /**
+     * 递归方法
+     *
+     * @param x
+     * @return
+     */
+    public static int recurse(int x) {
+        // 具有访问出口，符合正确递归调用方法理念
+        if (x >= Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        return recurse(x++);
+    }
+
+    public static void main(String[] args) {
+        recurse(Integer.MIN_VALUE);
+    }
+
+}
