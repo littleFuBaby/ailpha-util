5b04a8f2bc93b284f73521c2990016e4cb43ce16
andy <yunsonglife@sina.com>
2022-12-06 20:57:46 +0800

    replaceAll by regex

---
+++ src/main/java/com/ailpha/practice/string/StringTest.java
@@ -0,0 +1,14 @@
+package com.ailpha.practice.string;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        String str = "dfa12321afdsdfdf23423sdf";
+        String[] array = str.split("[a-z]+");
+        for (String s : array) {
+            System.out.println(s);
+        }
+
+    }
+
+}
