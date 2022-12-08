97ad5c494638c7f40311235011e9b4a85854ec57
andy <yunsonglife@sina.com>
2022-12-08 14:58:50 +0800

    update StringTest

--- src/main/java/com/ailpha/practice/string/StringTest.java
+++ src/main/java/com/ailpha/practice/string/StringTest.java
@@ -3,7 +3,7 @@ package com.ailpha.practice.string;
 public class StringTest {
 
     public static void main(String[] args) {
-        String str = "dfa12321afdsdfdf23423sdf";
+        String str = "df2321afdsdfdf23423sdf";
         String[] array = str.split("[a-z]+");
         for (String s : array) {
             System.out.println(s);
