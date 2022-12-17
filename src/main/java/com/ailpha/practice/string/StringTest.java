6157f36f41cda6c5672a1ef340cf3410d661c44f
andy <yunsonglife@sina.com>
2022-12-17 22:51:53 +0800

    commit new code

--- src/main/java/com/ailpha/practice/string/StringTest.java
+++ src/main/java/com/ailpha/practice/string/StringTest.java
@@ -17,5 +17,28 @@ public class StringTest {
         for (char ch : str.toCharArray()) {
             System.out.println(ch);
         }
+
+        String x = "abc";
+        String y = new String("abc");
+
+        System.out.println(x == y);
+
+        System.out.println(x.equals(y));
+
+        System.out.println(x.equalsIgnoreCase(y));
+
+        System.out.println(String.valueOf('A' + 32));
+
+        StringBuffer sb = new StringBuffer();
+        sb.append('A' + 32);
+        System.out.println(sb.toString());
+
+        System.out.println(str.toLowerCase());
+
+        int number = 5; //注意这里数字只能是个位数并且不能为负
+        String temp = Integer.toString(number);
+        char ch = temp.charAt(0);
+        System.out.println("数字转字符："+ch);
+
     }
 }
