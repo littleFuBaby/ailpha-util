e25bc4ab915a30cb2e3c06ff0b187b77248119ff
andy <yunsonglife@sina.com>
2022-12-20 00:13:25 +0800

    add new code

---
+++ src/main/java/com/ailpha/practice/array/ArrayListDemo.java
@@ -0,0 +1,11 @@
+package com.ailpha.practice.array;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+    }
+}
--- src/main/java/com/ailpha/practice/array/CollectionsInstance.java
+++ src/main/java/com/ailpha/practice/array/CollectionsInstance.java
@@ -6,15 +6,16 @@ import java.util.Map;
 
 public class CollectionsInstance {
 
-	public static void main(String[] args) {
-		Map<String, String> map = new HashMap<>();
-		map.put("fu", "0098");
-		map.put("yun", "009");
-		map.put("song", "00");
-		System.out.println(map);
-		
-		Map<String, String> newMap = Collections.singletonMap("fuyunsong", "0000");
-		System.out.println(newMap);
-	}
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("fu", "0098");
+        map.put("yun", "009");
+        map.put("song", "00");
+        System.out.println(map);
+
+        Map<String, String> newMap = Collections.singletonMap("fuyunsong", "0000");
+        System.out.println(newMap);
+    }
 
 }
+
