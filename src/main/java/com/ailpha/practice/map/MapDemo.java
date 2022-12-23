11cb6e35b1399178aea2181260c3e0540726e8a1
andy <yunsonglife@sina.com>
2022-12-23 18:09:25 +0800

    add map code

---
+++ src/main/java/com/ailpha/practice/map/MapDemo.java
@@ -0,0 +1,23 @@
+package com.ailpha.practice.map;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MapDemo {
+
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+
+        Map<String, Object> map2 = new TreeMap<>();
+
+        Map<String, Object> map3 = new Hashtable<>();
+
+        Map<String, Object> map4 = new ConcurrentHashMap<>();
+
+        Map<String, Object> map5 = new LinkedHashMap<>();
+
+        Properties properties = new Properties();
+
+    }
+
+}
